31e4a1061503f2eb2664eb2dc98c492aecb4b462
LBW <1021627950@qq.com>
2018-09-10 16:57:54 +0800

    This Test commit2.0

---
+++ Test2.java
@@ -0,0 +1,10 @@
+/**
+ * @author：LBW
+ * @date 2018/9/10 16:54
+ * @description TODO
+ */
+public class Test2 {
+    public  static void  main(String[] args){
+        System.out.println("that's ok");
+    }
+}
